404c32a9400a3f7c85940d27ff179ec135ee2abb
amiranify <amiranify@mail.ru>
2022-11-11 09:23:08 +0300

    add removeIf

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Иван", "Иванов", 15));
         people.add(new Person("Пётр", "Петров", 20));
-        people.add(new Person("Василий", "Васильев", 19));
-        people.add(new Person("Барон", "фон Трипербах", 25));
+        people.add(new Person("Василий", "Гасан Абдурахман ибн Хоттаб", 19));
+        people.add(new Person("Барон", "фон Трипербах Лепёхин", 25));
+        people.add(new Person("Михаил", "Салтыков-Щедрин", 24));
+        people.add(new Person("Маша", "Сидорова", 12));
         System.out.println(people);
 
         Collections.sort(people, new PersonComparat(3));
+        System.out.println(people);
+
+       people.removeIf(person -> person.getAge() < 18 );
         System.out.println(people);
     }
 }
